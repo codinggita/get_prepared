9faca22bfe88c73d803aa36616609fb0a9602f64
Chayan-Goyal <chayangoyal2004@gmail.com>
2024-02-02 11:18:45 +0530

    files added

---
+++ refer.js
@@ -0,0 +1,12 @@
+function refer()
+{
+    var email =document.myform.email.value;
+     if(email==null || email=="")
+     {
+         alert("Please Enter Your Email ");
+     }
+     else
+     {
+         alert("We will Contact Your Friend As Soon As Possible");
+     }
+}
