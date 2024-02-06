25767b8af8438a6e21139803152ebb923f1bd7c7
Chayan-Goyal <chayangoyal2004@gmail.com>
2024-02-07 00:38:04 +0530

    added the react index file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,13 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import Header from "./components/Header";
 import "./App.css";
-import Navbar from "./components/dashboard/navbar";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-        <Navbar/>
+        <Header/>
     </>
   );
 }
